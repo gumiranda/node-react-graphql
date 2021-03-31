d2121f902d147fae223c2df1aa0c7faaf7663578
gumiranda <imartytk@hotmail.com>
2021-03-31 17:13:18 -0300

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+		es2020: true,
+		node: true,
+	},
+	extends: ['airbnb-base'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'import/prefer-default-export': 'off',
+		'no-underscore-dangle': 'off',
+		'no-undef': 'off',
+		indent: 'off',
+		'max-classes-per-file': 'off',
+		'import/no-unresolved': 'off',
+		'import/extensions': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'no-tabs': 'off',
+	},
+};
