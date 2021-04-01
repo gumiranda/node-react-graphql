5731e1f7591ec99a367c76eabed4596b673fafbf
gumiranda <imartytk@hotmail.com>
2021-04-01 16:17:56 -0300

    presentation layer

---
+++ src/presentation/controllers/load-last-ranking.ts
@@ -0,0 +1,21 @@
+import {
+	Controller,
+	HttpResponse,
+	serverError,
+	ok,
+} from '@/presentation/contracts';
+import { RankingScoreViewModel } from '@/presentation/view-models';
+import { LastRankingLoader } from '@/domain/usecases';
+
+export class LoadLastRankingController implements Controller {
+	constructor(private readonly lastRankingLoader: LastRankingLoader) {}
+
+	async handle(): Promise<HttpResponse<RankingScoreViewModel[]>> {
+		try {
+			const ranking = await this.lastRankingLoader.load();
+			return ok(RankingScoreViewModel.mapCollection(ranking));
+		} catch (error) {
+			return serverError(error);
+		}
+	}
+}
