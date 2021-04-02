4b43a661bca0218431f39f38b378215039e0059e
gumiranda <imartytk@hotmail.com>
2021-04-02 20:11:30 -0300

    centralize cards

--- client-app/src/pages/Home/styles.ts
+++ client-app/src/pages/Home/styles.ts
@@ -2,55 +2,6 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 import colors from '../../utils/colors';
 
-export const Container = styled.div`
-  display: flex;
-  @media (max-width: 1080px) {
-    flex-direction: row;
-  }
-  @media (max-width: 639px) {
-    flex-direction: column;
-    align-self: center;
-  }
-`;
-export const Title = styled.h1`
-  font-size: 3rem;
-  margin-top: 2.5rem;
-  margin-right: 1rem;
-  color: ${colors.white};
-  margin-left: 1rem;
-  line-height: 3.5rem;
-`;
-export const Form = styled.form`
-  margin-left: 1rem;
-  margin-right: 1rem;
-  margin-top: 2.5rem;
-  max-width: 43.75rem;
-  display: flex;
-  input {
-    flex: 1;
-    height: 3.375rem;
-    padding: 0 1.5rem;
-    border: 0;
-    border-radius: 0.3125rem 0 0 0.3125rem;
-    color: ${colors.gray};
-    &::placeholder {
-      color: ${colors.lightGray};
-    }
-  }
-  button {
-    width: 13.125rem;
-    height: 3.375rem;
-    background: ${colors.green};
-    border-radius: 0 0.3125rem 0.3125rem 0;
-    color: ${colors.white};
-    border: 0;
-    font-weight: bold;
-    transition: background-color 0.2s;
-    &:hover {
-      background: ${shade(0.2, colors.green)};
-    }
-  }
-`;
 export const Grid = styled.div`
   display: inline-grid;
   @media (min-width: 1301px) {
@@ -62,36 +13,26 @@ export const Grid = styled.div`
   @media (max-width: 899px) {
     grid-template-columns: auto auto;
   }
-  @media (max-width: 581px) {
+  @media (max-width: 520px) {
     grid-template-columns: auto;
-    padding: 0 11rem;
   }
-  @media (max-width: 482px) {
-    grid-template-columns: auto;
-    padding: 0 8rem;
-  }
-  @media (max-width: 412px) {
-    grid-template-columns: auto;
-    padding: 0 5.5rem;
-  }
-  @media (max-width: 380px) {
-    grid-template-columns: auto;
-    padding: 0 3.4rem;
-  }
-  @media (max-width: 330px) {
-    grid-template-columns: auto;
-    padding: 0 2.2rem;
-  }
-  padding: 1rem;
+  margin: 0 1rem;
+  grid-gap: 4.5rem;
+`;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const Card = styled.div`
-  margin: 1.8rem;
-  max-width: 43.75rem;
+  max-width: 44.75rem;
+  margin-top: 3.75rem;
+
   a {
     background: ${colors.white};
     border-radius: 0.3125rem;
     width: 98%;
-    padding: 1.5rem;
+    padding: 1.8rem;
     text-decoration: none;
     display: flex;
     align-items: center;
