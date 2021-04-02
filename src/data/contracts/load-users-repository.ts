4bb2f4e83eb2501c147ebd0bd18d7891d4d89e3e
gumiranda <imartytk@hotmail.com>
2021-04-02 10:02:42 -0300

    delete inutil files

--- src/data/contracts/load-users-repository.ts
+++
@@ -1,5 +0,0 @@
-import { UserModel } from '@/data/models';
-
-export interface LoadUsersRepository {
-	loadUsers: () => Promise<UserModel[]>;
-}
--- src/domain/usecases/load-users.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '@/domain/entities';
-
-export interface LoadUsers {
-	load: () => Promise<User[]>;
-}
