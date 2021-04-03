4dffb72d1b3808135634d3e555bf7ee52b6b8e84
gumiranda <imartytk@hotmail.com>
2021-04-03 19:04:21 -0300

    remove unused import

--- client-app/src/pages/UserDetails/UserDetails.tsx
+++ client-app/src/pages/UserDetails/UserDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from 'components/Layout/Layout';
 import { useParams, useHistory } from 'react-router-dom';
 import { useGetUsers } from 'graphql/useRequest';
